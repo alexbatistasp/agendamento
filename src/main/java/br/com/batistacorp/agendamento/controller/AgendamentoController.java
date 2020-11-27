ce1ae6777d15034edab9c978dc6f4f8ca8dab84a
alexbatistasp <alex.batistasp@yahoo.com.br>
2020-11-27 14:11:38 -0300

    Commit prop

--- src/main/java/br/com/batistacorp/agendamento/controller/AgendamentoController.java
+++ src/main/java/br/com/batistacorp/agendamento/controller/AgendamentoController.java
@@ -1,6 +1,7 @@
 package br.com.batistacorp.agendamento.controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.batistacorp.agendamento.dao.AgendamentoDAO;
+import br.com.batistacorp.agendamento.model.Agencia;
 import br.com.batistacorp.agendamento.model.Agendamento;
 
 @RestController
@@ -33,27 +35,54 @@ public class AgendamentoController {
 	}
 
 	@GetMapping("/agendamentos")
-	public ArrayList<Agendamento> buscar(@RequestParam int mode, @RequestParam String nome,
-			@RequestParam LocalDate data, @RequestParam int idAgencia) {
-
-		ArrayList<Agendamento> lista = null;
-		switch (mode) {
+	public ArrayList<Agendamento> buscar(@RequestParam int mode, 
+			                             @RequestParam String nome, 
+			                             @RequestParam String dataAg, 
+			                             @RequestParam int idAgencia){
+		Agencia ag;
+		ArrayList<Agendamento> lista=null;
+		LocalDate data;
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		switch(mode) {
 		case 0:
-			lista = (ArrayList<Agendamento>) dao.findAll();
+			lista = (ArrayList<Agendamento>)dao.findAll();
 			break;
 		case 1:
+			ag = new Agencia();
+			ag.setId(idAgencia);
+			lista = dao.findAllByAgencia(ag);
+			break;
 		case 2:
+			data = LocalDate.parse(dataAg, formatter); 
 			lista = dao.findAllByDataAgendamento(data);
 			break;
 		case 3:
+			ag = new Agencia();
+			ag.setId(idAgencia);
+			data = LocalDate.parse(dataAg, formatter);
+			lista = dao.findAllByDataAgendamentoAndAgencia(data, ag);
+			break;
 		case 4:
 			lista = dao.findAllByNomeClienteContains(nome);
 			break;
 		case 5:
+			ag = new Agencia();
+			ag.setId(idAgencia);
+			lista = dao.findAllByAgenciaAndNomeClienteContains(ag, nome);
+			break;
 		case 6:
+			data = LocalDate.parse(dataAg, formatter);
+			lista = dao.findAllByDataAgendamentoAndNomeClienteContains(data, nome);
+			break;
 		case 7:
+			ag = new Agencia();
+			ag.setId(idAgencia);
+			data = LocalDate.parse(dataAg, formatter);
+			lista = dao.findAllByAgenciaAndDataAgendamentoAndNomeClienteContains(ag, data, nome);
+			break;
 		}
-
+				
 		return lista;
 	}
 
--- src/main/java/br/com/batistacorp/agendamento/dao/AgendamentoDAO.java
+++ src/main/java/br/com/batistacorp/agendamento/dao/AgendamentoDAO.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.batistacorp.agendamento.model.Agencia;
 import br.com.batistacorp.agendamento.model.Agendamento;
 
 public interface AgendamentoDAO extends CrudRepository<Agendamento, Integer> {
 	
 	public ArrayList<Agendamento> findAllByDataAgendamento(LocalDate data);
 	public ArrayList<Agendamento> findAllByNomeClienteContains(String nome);
-
+	public ArrayList<Agendamento> findAllByAgencia(Agencia agencia);
+	public ArrayList<Agendamento> findAllByDataAgendamentoAndAgencia(LocalDate data, Agencia agencia); 
+	public ArrayList<Agendamento> findAllByDataAgendamentoAndNomeClienteContains(LocalDate data, String cliente);
+    public ArrayList<Agendamento> findAllByAgenciaAndNomeClienteContains(Agencia agencia, String cliente);
+    public ArrayList<Agendamento> findAllByAgenciaAndDataAgendamentoAndNomeClienteContains(Agencia agencia, LocalDate data, String cliente);
 
 }
